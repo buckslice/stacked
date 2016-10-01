780912b494769dd1dadd88daf018654d8979688b
Reddeyfish <edrichd@uci.edu>
2016-10-01 10:42:32 -0700

    load next level button uses the start key on a controller

---
+++ Mousers/Assets/Project/Utility/UIAnyKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Allows buttons to be activated by any keyboard action. Place on the same gameObject as the button.
+/// </summary>
+public class UIAnyKey : MonoBehaviour
+{
+    protected virtual void Update()
+    {
+        if (Input.anyKeyDown)
+            SendEvent();
+
+    }
+
+    protected virtual void SendEvent() //can override this for different functionality
+    {
+        ExecuteEvents.Execute(this.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler); //hook into the button's code, and cause it to act as if it was pressed
+    }
+}
+
---
+++ Mousers/Assets/Project/Utility/UIKeyboardShortcut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Activates a button based on a key press.
+/// </summary>
+public class UIKeyboardShortcut : UIAnyKey
+{
+    [SerializeField]
+    public KeyCode key;
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (Input.GetKeyDown(key))
+            SendEvent();
+    }
+}
+
