0c7f770e18066f0d44f9c8d14ef9a4d5cc2f1bbc
Reddeyfish <edrichd@uci.edu>
2016-10-04 17:59:15 -0700

    Ability component for boss abilities

---
+++ Mousers/Assets/Project/Player/Game Logic/Abilities/AlwaysTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// An ability that is always triggering. Useful for passive or always-on abilities
+/// </summary>
+public class AlwaysTrigger : MonoBehaviour, IAbilityTrigger {
+
+    public event AbilityTrigger abilityTriggerEvent = delegate { };
+
+    void Update()
+    {
+        abilityTriggerEvent();
+    }
+}
---
+++ Mousers/Assets/Project/Player/Game Logic/Abilities/Components/SpawnAbility.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// An action which spawns a pooled object
+/// </summary>
+public class SpawnAbility : AbstractAbilityAction {
+
+    [SerializeField]
+    protected string prefabName;
+
+    /// <summary>
+    /// Use PhotonNetwork.Instantiate if yes, else use SimplePool.Spawn.
+    /// </summary>
+    [SerializeField]
+    protected bool networkedPrefab;
+
+    public override void Activate()
+    {
+        PhotonNetwork.Instantiate(prefabName, transform.position, transform.rotation, 0);
+    }
+
+    public override void ActivateWithData(object data)
+    {
+        Activate();
+    }
+
+    public override void ActivateRemote()
+    {
+    }
+}
