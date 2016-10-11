d0c253963e3ce68f0d12661a9ae0a7fcc15d6b0e
jfc3 <buckslice@gmail.com>
2016-10-11 00:51:58 -0700

    Added circle shaped zone and improved zone scaling / function

--- Assets/Project/Environment/Telegraphing/Zone.cs
+++ Assets/Project/Environment/Telegraphing/Zone.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ZoneShape {
+    SQUARE,
+    CIRCLE
+}
+
 public enum ZoneType {
     HEALING_OVER_TIME,
     DAMAGE_OVER_TIME,
@@ -13,55 +18,87 @@ public class Zone : MonoBehaviour {
     public float healthChange = 1.0f;
     public float duration = 3.0f;
 
-    private float durTimer = 0.0f;
+    float durTimer = 0.0f;
+    ZoneShape shape;
 
     // TODO : change to max player count, also pool these zones
-    private Collider[] buffer = new Collider[10];
-    private BoxCollider col;
-    private ParticleSystem ps;
+    Collider[] buffer = new Collider[10];
+    Collider col;
+    ParticleSystem ps;
+
+    bool dying = false;
 
     // Use this for initialization
     void Awake() {
-        col = GetComponent<BoxCollider>();
+        col = GetComponent<Collider>();
+        if(col.GetType() == typeof(BoxCollider)) {  // square uses short box collider 
+            shape = ZoneShape.SQUARE;
+        } else {    // circle uses sphere collider for now, could add in custom puck shaped collider
+            shape = ZoneShape.CIRCLE;
+        }
         ps = GetComponentInChildren<ParticleSystem>();
     }
 
     // sets position and size of zone
     // position is specified by the bottom center
     // size is width height length
-    public void Setup(Vector3 position, Vector3 size) {
+    public void Setup(Vector3 position, Vector3 scale) {
+        if(shape == ZoneShape.CIRCLE) { // makes sure scale vector for circles is uniform
+            float maxDim = Mathf.Max(scale.x, Mathf.Max(scale.y, scale.z));
+            scale = Vector3.one * maxDim;
+        }
+
+        // calculate original bounding volume of zone
+        Vector3 origScale = transform.localScale;
+        Vector3 origSize = col.bounds.size;
+        float origVolume = origSize.x * 1.0f * origSize.z; // basically just an area for now
+
+        // set new position and scale
         transform.position = position;
-        transform.localScale = size;
-        CalculateParticleEmission();
+        transform.localScale = scale;
+
+        Vector3 newSize = col.bounds.size;
+        float newVolume = newSize.x * 1.0f * newSize.z;
+
+        // scales the particle system to the volume of the collider
+        // but bases it off of old volume and settings
+        ParticleSystem.EmissionModule psem = ps.emission;
+        float newRate = psem.rate.constant * newVolume / origVolume;
+        psem.rate = newRate;
+        ps.maxParticles = (int)(newRate * ps.startLifetime);
+
         ps.Play();  // play on awake is disabled since emission rate is being set after initialization
         // this is so the prewarm option will work correctly
     }
 
-    // set emission rate based on volume of collider
-    void CalculateParticleEmission() {
-        ParticleSystem.EmissionModule psem = ps.emission;
-        Vector3 size = col.bounds.size;
-        float volume = size.x * size.y * size.z;
-        psem.rate = volume;
-    }
-
     // Update is called once per frame
     void Update() {
+        if (dying) {
+            return;
+        }
+
         durTimer += Time.deltaTime;
         if (durTimer >= duration) {
             if (type == ZoneType.EXPLODE_AFTER_TIMER) {
                 DamageAllPlayersInContact();
             }
-            Destroy(gameObject);
+            DestroyZone();
         }
     }
 
     void DamageAllPlayersInContact() {
-        // calculate worldspace box where collider is
-        Vector3 half = transform.localScale / 2.0f;
-        Vector3 worldCenter = transform.position + Vector3.up * half.y;
+        int count = 0;
 
-        int count = Physics.OverlapBoxNonAlloc(worldCenter, half, buffer);
+        if (shape == ZoneShape.SQUARE) {
+            Vector3 half = transform.localScale / 2.0f;
+            Vector3 worldCenter = transform.position + Vector3.up * half.y;
+            count = Physics.OverlapBoxNonAlloc(worldCenter, half, buffer);
+        } else {
+            Vector3 scale = transform.localScale;
+            float radius = Mathf.Max(scale.x, Mathf.Max(scale.y, scale.z));
+            count = Physics.OverlapSphereNonAlloc(transform.position, radius, buffer);
+        }
+        
         for (int i = 0; i < count; ++i) {
             if (buffer[i].CompareTag(Tags.Player)) {
                 buffer[i].GetComponent<Damageable>().Damage(healthChange);
@@ -69,19 +106,27 @@ public class Zone : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter(Collision c) {
-        if (type == ZoneType.EXPLODE_ON_CONTACT && c.collider.CompareTag(Tags.Player)) {
+    void OnTriggerEnter(Collider c) {
+        if (type == ZoneType.EXPLODE_ON_CONTACT && c.CompareTag(Tags.Player)) {
             DamageAllPlayersInContact();
-            Destroy(gameObject);
+            DestroyZone();
         }
     }
 
-    void OnCollisionStay(Collision c) {
+    void OnTriggerStay(Collider c) {
         if ((type == ZoneType.DAMAGE_OVER_TIME || type == ZoneType.HEALING_OVER_TIME) 
-            && c.collider.CompareTag(Tags.Player)) {
-            c.collider.GetComponent<Damageable>().Damage(healthChange);
+            && c.CompareTag(Tags.Player)) {
+            c.GetComponent<Damageable>().Damage(healthChange * Time.fixedDeltaTime);
         }
     }
 
-
+    // destroys this object after the particle system runs its course
+    void DestroyZone() {
+        if (!dying) {
+            dying = true;
+            Destroy(col);
+            ps.Stop();
+            Destroy(gameObject, ps.startLifetime);
+        }
+    }
 }
