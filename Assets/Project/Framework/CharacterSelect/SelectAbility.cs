74bc8deeb818245dade6c40c4eeae45c34c838f4
Reddeyfish <edrichd@uci.edu>
2016-10-13 18:18:11 -0700

    Cursor turns green when a selection is made

--- Assets/Project/Framework/CharacterSelect/SelectAbility.cs
+++ Assets/Project/Framework/CharacterSelect/SelectAbility.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 
 public class SelectAbility : AbstractAbilityAction {
-    private bool selected;
+    bool _selected = false;
+    private bool selected {
+        get { return _selected; }
+        set {
+            _selected = value;
+            if (_selected) {
+                visuals.color = Color.green;
+            }
+        }
+    }
     private CharacterSelectCursor cursor;
     PlayerInputHolder inputHolder;
+    SpriteRenderer visuals;
 
     protected override void Start()
     {
         base.Start();
         cursor = GetComponentInParent<CharacterSelectCursor>();
         inputHolder = GetComponentInParent<PlayerInputHolder>();
+        visuals = GetComponentInParent<SpriteRenderer>();
         selected = false;
     }
 
