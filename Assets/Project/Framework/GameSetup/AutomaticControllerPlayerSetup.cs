389a26e7fa91b55b6668bc7e787662c27a7b8a59
Reddeyfish <edrichd@uci.edu>
2016-10-15 10:32:36 -0700

    Add automatic controller player setup

---
+++ Assets/Project/Framework/GameSetup/AutomaticControllerPlayerSetup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Intended to be used for development only, in order to mock a player isntead of going through character select.
+/// </summary>
+public class AutomaticControllerPlayerSetup : PlayerSetup {
+    protected override void Awake() {
+        base.Awake();
+        PlayerSetup[] otherPlayerSetups = GameObject.FindObjectsOfType<PlayerSetup>();
+        foreach (PlayerSetup otherPlayerSetup in otherPlayerSetups) {
+            if (this != otherPlayerSetup) {
+                DestroyImmediate(this.transform.root.gameObject);
+                return;
+            }
+        }
+        inputBindings = new ControllerPlayerInput();
+
+        Assert.IsTrue(R41DNetworking.Main.NetworkingMode != R41DNetworkingMode.ONLINE, "Automatic Setups will not work correctly over the network."); //we aren't hooked up correctly for online
+    }
+}
