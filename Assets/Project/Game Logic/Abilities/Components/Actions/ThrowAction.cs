126263e31a2d6ed1a314086c6b50a5c5605d9d61
rgammad <rgammad@uci.edu>
2016-10-17 15:59:16 -0700

    Altered File Structure
    moved things around and deleted stuff

---
+++ Assets/Project/Game Logic/Abilities/Components/Actions/ThrowAction.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ThrowAction : TypedTargetedAbilityAction {
+
+    [SerializeField]
+    protected float throwDistance = 10;
+
+    [SerializeField]
+    protected float throwDuration = 1;
+
+    [SerializeField]
+    protected GameObject thrownObjectAbilityPrefab;
+
+    IDamageTracker trackerReference;
+
+    protected override void Start() {
+        base.Start();
+        trackerReference = GetComponentInParent<IDamageHolder>().GetRootDamageTracker();
+    }
+
+    public override bool isAbilityActivatible(GameObject target) {
+        if (target.GetComponentInParent<Rigidbody>() == null) {
+            return false;
+        }
+
+        if (target.GetComponentInParent<IMovement>() == null) {
+            return false;
+        }
+
+        if (target.GetComponentInParent<AbilityNetworking>() == null) {
+            return false;
+        }
+
+        IDamageTracker targetReference = target.GetComponentInParent<IDamageHolder>().GetRootDamageTracker();
+        return targetReference != trackerReference;
+    }
+
+    public override bool Activate(GameObject context, PhotonStream stream) {
+        Vector3 destinationPosition = transform.position + throwDistance * Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+        AbilityNetworking targetNetworking = context.GetComponentInParent<AbilityNetworking>();
+        IMovement targetMovement = context.GetComponentInParent<IMovement>();
+        Rigidbody targetRigid = context.GetComponentInParent<Rigidbody>();
+
+        GameObject instantiatedThrownObjectAbility = SimplePool.Spawn(thrownObjectAbilityPrefab);
+        ThrownObjectAbility thrownObjectAbility = instantiatedThrownObjectAbility.GetComponent<ThrownObjectAbility>();
+        thrownObjectAbility.Initialize(transform.position, destinationPosition, Time.time + throwDuration, targetNetworking, targetMovement, targetRigid);
+        return true;
+    }
+}
