13574ac174c31f3493d5ff450311abd11ca956a8
Reddeyfish <edrichd@uci.edu>
2016-10-19 10:15:18 -0700

    Player models are colored

---
+++ Assets/Project/Art/Player/TintedModel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(DamageHolder))]
+public class TintedModel : MonoBehaviour {
+
+    [SerializeField]
+    protected Renderer[] tintedRenderers;
+	void Start () {
+        Player player = (Player)GetComponent<DamageHolder>().GetRootDamageTracker();
+
+        if (player.PlayerID < Player.playerColoring.Length && player.PlayerID >= 0) {
+
+            foreach (Renderer tintedRenderer in tintedRenderers) {
+                tintedRenderer.material.color = Player.playerColoring[player.PlayerID];
+            }
+        }
+	}
+}
