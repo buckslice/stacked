dd65c66bbbeef5d7aeba285b2970f89e6ed05ffd
verythinice <quake.grizzly@gmail.com>
2016-10-24 23:27:46 -0700

    Changing tags and player input in preparation for rebinding

--- Assets/Project/Utility/Tags.cs
+++ Assets/Project/Utility/Tags.cs
@@ -46,10 +46,31 @@ public static class Tags {
         public const KeyCode Start = KeyCode.Return;
         public const KeyCode BasicAttack = KeyCode.LeftShift; 
         public const KeyCode Ability1 = KeyCode.Q; 
-        public const KeyCode Ability2 = KeyCode.E; 
+        public const KeyCode Ability2 = KeyCode.E;
 
         public static class Joystick1
         {
+            public const KeyCode button0 = KeyCode.Joystick1Button0;
+            public const KeyCode button1 = KeyCode.Joystick1Button1;
+            public const KeyCode button2 = KeyCode.Joystick1Button2;
+            public const KeyCode button3 = KeyCode.Joystick1Button3;
+            public const KeyCode button4 = KeyCode.Joystick1Button4;
+            public const KeyCode button5 = KeyCode.Joystick1Button5;
+            public const KeyCode button6 = KeyCode.Joystick1Button6;
+            public const KeyCode button7 = KeyCode.Joystick1Button7;
+            public const KeyCode button8 = KeyCode.Joystick1Button8;
+            public const KeyCode button9 = KeyCode.Joystick1Button9;
+            public const KeyCode button10 = KeyCode.Joystick1Button10;
+            public const KeyCode button11 = KeyCode.Joystick1Button11;
+            public const KeyCode button12 = KeyCode.Joystick1Button12;
+            public const KeyCode button13 = KeyCode.Joystick1Button13;
+            public const KeyCode button14 = KeyCode.Joystick1Button14;
+            public const KeyCode button15 = KeyCode.Joystick1Button15;
+            public const KeyCode button16 = KeyCode.Joystick1Button16;
+            public const KeyCode button17 = KeyCode.Joystick1Button17;
+            public const KeyCode button18 = KeyCode.Joystick1Button18;
+            public const KeyCode button19 = KeyCode.Joystick1Button19;
+
             public const string HorizontalMovement = "J1_HorizontalMovement";
             public const string VerticalMovement = "J1_VerticalMovement";
             public const string HorizontalAiming = "J1_HorizontalAiming";
@@ -57,16 +78,32 @@ public static class Tags {
             public const string LeftTrigger = "J1_LeftTrigger";
             public const string RightTrigger = "J1_RightTrigger";
 
-            public const KeyCode Start = KeyCode.Joystick1Button7;
-            public const KeyCode AButton = KeyCode.Joystick1Button0;
-            public const KeyCode BButton = KeyCode.Joystick1Button1;
-            public const KeyCode XButton = KeyCode.Joystick1Button2;
-            public const KeyCode YButton = KeyCode.Joystick1Button3;
-            public const KeyCode LeftBumper = KeyCode.Joystick1Button4;
-            public const KeyCode RightBumper = KeyCode.Joystick1Button5;
+            public static readonly KeyCode[] allButtons = { button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19 };
+            public static readonly string[] allAxes = { HorizontalMovement, VerticalMovement, HorizontalAiming, VerticalAiming, LeftTrigger, RightTrigger };
         }
         public static class Joystick2
         {
+            public const KeyCode button0 = KeyCode.Joystick2Button0;
+            public const KeyCode button1 = KeyCode.Joystick2Button1;
+            public const KeyCode button2 = KeyCode.Joystick2Button2;
+            public const KeyCode button3 = KeyCode.Joystick2Button3;
+            public const KeyCode button4 = KeyCode.Joystick2Button4;
+            public const KeyCode button5 = KeyCode.Joystick2Button5;
+            public const KeyCode button6 = KeyCode.Joystick2Button6;
+            public const KeyCode button7 = KeyCode.Joystick2Button7;
+            public const KeyCode button8 = KeyCode.Joystick2Button8;
+            public const KeyCode button9 = KeyCode.Joystick2Button9;
+            public const KeyCode button10 = KeyCode.Joystick2Button10;
+            public const KeyCode button11 = KeyCode.Joystick2Button11;
+            public const KeyCode button12 = KeyCode.Joystick2Button12;
+            public const KeyCode button13 = KeyCode.Joystick2Button13;
+            public const KeyCode button14 = KeyCode.Joystick2Button14;
+            public const KeyCode button15 = KeyCode.Joystick2Button15;
+            public const KeyCode button16 = KeyCode.Joystick2Button16;
+            public const KeyCode button17 = KeyCode.Joystick2Button17;
+            public const KeyCode button18 = KeyCode.Joystick2Button18;
+            public const KeyCode button19 = KeyCode.Joystick2Button19;
+
             public const string HorizontalMovement = "J2_HorizontalMovement";
             public const string VerticalMovement = "J2_VerticalMovement";
             public const string HorizontalAiming = "J2_HorizontalAiming";
@@ -74,16 +111,32 @@ public static class Tags {
             public const string LeftTrigger = "J2_LeftTrigger";
             public const string RightTrigger = "J2_RightTrigger";
 
-            public const KeyCode Start = KeyCode.Joystick2Button7;
-            public const KeyCode AButton = KeyCode.Joystick2Button0;
-            public const KeyCode BButton = KeyCode.Joystick2Button1;
-            public const KeyCode XButton = KeyCode.Joystick2Button2;
-            public const KeyCode YButton = KeyCode.Joystick2Button3;
-            public const KeyCode LeftBumper = KeyCode.Joystick2Button4;
-            public const KeyCode RightBumper = KeyCode.Joystick2Button5;
+            public static readonly KeyCode[] allButtons = { button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19 };
+            public static readonly string[] allAxes = { HorizontalMovement, VerticalMovement, HorizontalAiming, VerticalAiming, LeftTrigger, RightTrigger };
         }
         public static class Joystick3
         {
+            public const KeyCode button0 = KeyCode.Joystick3Button0;
+            public const KeyCode button1 = KeyCode.Joystick3Button1;
+            public const KeyCode button2 = KeyCode.Joystick3Button2;
+            public const KeyCode button3 = KeyCode.Joystick3Button3;
+            public const KeyCode button4 = KeyCode.Joystick3Button4;
+            public const KeyCode button5 = KeyCode.Joystick3Button5;
+            public const KeyCode button6 = KeyCode.Joystick3Button6;
+            public const KeyCode button7 = KeyCode.Joystick3Button7;
+            public const KeyCode button8 = KeyCode.Joystick3Button8;
+            public const KeyCode button9 = KeyCode.Joystick3Button9;
+            public const KeyCode button10 = KeyCode.Joystick3Button10;
+            public const KeyCode button11 = KeyCode.Joystick3Button11;
+            public const KeyCode button12 = KeyCode.Joystick3Button12;
+            public const KeyCode button13 = KeyCode.Joystick3Button13;
+            public const KeyCode button14 = KeyCode.Joystick3Button14;
+            public const KeyCode button15 = KeyCode.Joystick3Button15;
+            public const KeyCode button16 = KeyCode.Joystick3Button16;
+            public const KeyCode button17 = KeyCode.Joystick3Button17;
+            public const KeyCode button18 = KeyCode.Joystick3Button18;
+            public const KeyCode button19 = KeyCode.Joystick3Button19;
+
             public const string HorizontalMovement = "J3_HorizontalMovement";
             public const string VerticalMovement = "J3_VerticalMovement";
             public const string HorizontalAiming = "J3_HorizontalAiming";
@@ -91,16 +144,32 @@ public static class Tags {
             public const string LeftTrigger = "J3_LeftTrigger";
             public const string RightTrigger = "J3_RightTrigger";
 
-            public const KeyCode Start = KeyCode.Joystick3Button7;
-            public const KeyCode AButton = KeyCode.Joystick3Button0;
-            public const KeyCode BButton = KeyCode.Joystick3Button1;
-            public const KeyCode XButton = KeyCode.Joystick3Button2;
-            public const KeyCode YButton = KeyCode.Joystick3Button3;
-            public const KeyCode LeftBumper = KeyCode.Joystick3Button4;
-            public const KeyCode RightBumper = KeyCode.Joystick3Button5;
+            public static readonly KeyCode[] allButtons = { button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19 };
+            public static readonly string[] allAxes = { HorizontalMovement, VerticalMovement, HorizontalAiming, VerticalAiming, LeftTrigger, RightTrigger };
         }
         public static class Joystick4
         {
+            public const KeyCode button0 = KeyCode.Joystick4Button0;
+            public const KeyCode button1 = KeyCode.Joystick4Button1;
+            public const KeyCode button2 = KeyCode.Joystick4Button2;
+            public const KeyCode button3 = KeyCode.Joystick4Button3;
+            public const KeyCode button4 = KeyCode.Joystick4Button4;
+            public const KeyCode button5 = KeyCode.Joystick4Button5;
+            public const KeyCode button6 = KeyCode.Joystick4Button6;
+            public const KeyCode button7 = KeyCode.Joystick4Button7;
+            public const KeyCode button8 = KeyCode.Joystick4Button8;
+            public const KeyCode button9 = KeyCode.Joystick4Button9;
+            public const KeyCode button10 = KeyCode.Joystick4Button10;
+            public const KeyCode button11 = KeyCode.Joystick4Button11;
+            public const KeyCode button12 = KeyCode.Joystick4Button12;
+            public const KeyCode button13 = KeyCode.Joystick4Button13;
+            public const KeyCode button14 = KeyCode.Joystick4Button14;
+            public const KeyCode button15 = KeyCode.Joystick4Button15;
+            public const KeyCode button16 = KeyCode.Joystick4Button16;
+            public const KeyCode button17 = KeyCode.Joystick4Button17;
+            public const KeyCode button18 = KeyCode.Joystick4Button18;
+            public const KeyCode button19 = KeyCode.Joystick4Button19;
+
             public const string HorizontalMovement = "J4_HorizontalMovement";
             public const string VerticalMovement = "J4_VerticalMovement";
             public const string HorizontalAiming = "J4_HorizontalAiming";
@@ -108,13 +177,8 @@ public static class Tags {
             public const string LeftTrigger = "J4_LeftTrigger";
             public const string RightTrigger = "J4_RightTrigger";
 
-            public const KeyCode Start = KeyCode.Joystick4Button7;
-            public const KeyCode AButton = KeyCode.Joystick4Button0;
-            public const KeyCode BButton = KeyCode.Joystick4Button1;
-            public const KeyCode XButton = KeyCode.Joystick4Button2;
-            public const KeyCode YButton = KeyCode.Joystick4Button3;
-            public const KeyCode LeftBumper = KeyCode.Joystick4Button4;
-            public const KeyCode RightBumper = KeyCode.Joystick4Button5;
+            public static readonly KeyCode[] allButtons = { button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19 };
+            public static readonly string[] allAxes = { HorizontalMovement, VerticalMovement, HorizontalAiming, VerticalAiming, LeftTrigger, RightTrigger };
         }
     }
 
