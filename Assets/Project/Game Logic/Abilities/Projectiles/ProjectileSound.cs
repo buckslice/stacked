cdf9b0b73169238eca856320a2de4eddb7c96548
Roldan Gammad <rgammad@uci.edu>
2016-10-31 16:30:43 -0700

    Added fireball sound file

---
+++ Assets/Project/Game Logic/Abilities/Projectiles/ProjectileSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProjectileSound : MonoBehaviour
+{
+
+    AudioSource target;
+    void Awake()
+    {
+        target = GetComponent<AudioSource>();
+        target.Play();
+    }
+
+}
