86c12b679e506c7831f200c8e18f46898be9e54f
Reddeyfish <edrichd@uci.edu>
2016-10-31 18:49:36 -0700

    Add sound scripting

--- Assets/Project/Game Logic/Abilities/Components/Actions/AbilitySound.cs
+++ Assets/Project/Game Logic/Abilities/Components/Actions/AbilitySound.cs
@@ -3,14 +3,17 @@ using UnityEngine.Assertions;
 using System.Collections;
 using System.Collections.Generic;
 
-public class AbilitySound : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+public class AbilitySound : AbstractAbilityAction
 {
-
     AudioSource target;
     void Awake()
     {
         target = GetComponent<AudioSource>();
-        target.Play();
     }
 
+    public override bool Activate(PhotonStream stream) {
+        target.Play();
+        return true;
+    }
 }
---
+++ Assets/Project/Game Logic/Abilities/Projectiles/SoundOnActive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundOnActive : ProjectileLifetimeAction {
+
+    AudioSource target;
+
+    protected override void Awake() {
+        target = GetComponent<AudioSource>();
+        base.Awake();
+    }
+
+    protected override void OnProjectileCreated() {
+        target.Play();
+    }
+}
