b9cf0fc3837b3d1dd0bd82da42ffb4de32b61e7a
verythinice <quake.grizzly@gmail.com>
2016-11-02 15:15:35 -0700

    Started on rebinding menu

---
+++ Assets/Project/Game Logic/Framework/Registration/UI/ButtonCheckMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonCheckMenu : MonoBehaviour {
+    public IPlayerInput bindings;
+
+    private const float BUTTON_FREEZE_DELAY = .1f;
+    private const float AXIS_FREEZE_DELAY = .2f;
+    private float noInputUntil = -1f;
+
+    private enum ButtonState
+    {
+        up,
+        down,
+        over
+    }
+
+    private struct MenuOption
+    {
+        public string name;
+        public Rect rect;
+        public Rect textRect;
+        public ButtonState state;
+    }
+    private MenuOption[] options;
+    private int selectedNo = 0;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
