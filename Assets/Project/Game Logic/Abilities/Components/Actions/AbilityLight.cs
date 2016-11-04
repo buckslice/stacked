0bcf8403016dbc3a90931ddd7f954d893951804e
Reddeyfish <edrichd@uci.edu>
2016-11-04 15:25:33 -0700

    Add lights to bright abilities

---
+++ Assets/Project/Game Logic/Abilities/Components/Actions/AbilityLight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AbilityLight : AbstractAbilityAction {
+
+    [SerializeField]
+    protected Light targetLight;
+
+    [SerializeField]
+    protected AnimationCurve intensityOverTime;
+
+    [SerializeField]
+    protected float intensity;
+
+    [SerializeField]
+    protected float duration;
+
+    public override bool Activate(PhotonStream stream) {
+        Callback.DoLerp((float l) => {
+            targetLight.intensity = intensity * intensityOverTime.Evaluate(l);
+        }, duration, this);
+        return true;
+    }
+}
