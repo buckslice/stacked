1833bfcdfa9a894fe574a8681c16d18830620a09
verythinice <quake.grizzly@gmail.com>
2016-11-05 23:37:30 -0700

    Fixed AutomaticControllerPlayerSetup

--- Assets/Project/Game Logic/Framework/GameSetup/AutomaticControllerPlayerSetup.cs
+++ Assets/Project/Game Logic/Framework/GameSetup/AutomaticControllerPlayerSetup.cs
@@ -18,8 +18,9 @@ public class AutomaticControllerPlayerSetup : PlayerSetup {
         }
 
         //I have no idea why this creates a null pointer exception when there exists more than one AutomaticSetup in the scene. Stuff still works even when the exception is thrown.
-        inputBindings = new ControllerPlayerInput();
-
+        ControllerPlayerInput input = new ControllerPlayerInput();
+        input.Start();
+        inputBindings = input;
         Assert.IsTrue(R41DNetworking.Main.NetworkingMode != R41DNetworkingMode.ONLINE, "Automatic Setups will not work correctly over the network."); //we aren't hooked up correctly for online
     }
 }
