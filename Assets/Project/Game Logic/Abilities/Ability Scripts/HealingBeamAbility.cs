f48c05a038c530c879e3907e9907477a52bad415
jfc3 <buckslice@gmail.com>
2016-11-07 20:29:03 -0800

    Add sound to healing beam ability

--- Assets/Project/Game Logic/Abilities/Ability Scripts/HealingBeamAbility.cs
+++ Assets/Project/Game Logic/Abilities/Ability Scripts/HealingBeamAbility.cs
@@ -6,6 +6,7 @@ public class HealingBeamAbility : DurationAbilityAction {
     public Collider col;
     public ParticleSystem beamParticles;
     public ParticleSystem sparkParticles;
+    public AudioSource source;
 
     protected override void Start() {
 
@@ -15,11 +16,13 @@ public class HealingBeamAbility : DurationAbilityAction {
         col.enabled = true;
         beamParticles.Play();
         sparkParticles.Play();
+        source.Play();
     }
 
     protected override void OnDurationEnd() {
         col.enabled = false;
         beamParticles.Stop();
         sparkParticles.Stop();
+        source.Stop();
     }
 }
