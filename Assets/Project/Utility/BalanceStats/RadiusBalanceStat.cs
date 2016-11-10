05c35fc36e10720c16937a7c2b73d77535c86101
Reddeyfish <edrichd@uci.edu>
2016-11-09 22:26:08 -0800

    Fix pre-existing balanceStat linkages

---
+++ Assets/Project/Utility/BalanceStats/RadiusBalanceStat.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RadiusBalanceStat : BalanceStat {
+
+    protected override StatType type {
+        get { return StatType.RADIUS; }
+    }
+}
