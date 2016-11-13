278353c922224117bfb4f4a2468693b86d00c1ce
jfc3 <buckslice@gmail.com>
2016-11-13 05:04:15 -0800

    Reduced visual clutter from fireball and made it look cooler

---
+++ Assets/Project/Game Logic/Abilities/Projectiles/ForwardSpinModelRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardSpinModelRotation : MonoBehaviour {
+
+    public float degreesPerSecond = 200.0f;
+
+    Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponentInParent<Rigidbody>();
+        Debug.Assert(rigid);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(rigid.transform.right, degreesPerSecond * Time.deltaTime, Space.World);
+	}
+}
