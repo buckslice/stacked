aa9b60b7938115be1e9bd5979ab831940d6b5293
Reddeyfish <edrichd@uci.edu>
2016-11-13 21:35:00 -0800

    Player has screen shake on death

---
+++ Assets/Project/Game Logic/Abilities/Components/Actions/ScreenShake.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : AbstractAbilityAction {
+
+    CameraShakeScript cameraShakeScript;
+
+    [SerializeField]
+    protected float screenShakeDuration = 0.5f;
+
+    protected override void Start() {
+        base.Start();
+        cameraShakeScript = Camera.main.GetComponent<CameraShakeScript>();
+    }
+
+    public override bool Activate(PhotonStream stream) {
+        cameraShakeScript.screenShake(screenShakeDuration);
+        return true;
+    }
+}
