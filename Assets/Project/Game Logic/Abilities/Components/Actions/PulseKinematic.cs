c969ed5a0596ec83aadc7c4e5f34bfe9fb6bc020
Reddeyfish <edrichd@uci.edu>
2016-11-16 15:46:09 -0800

    Add upper 2 levels of boss

---
+++ Assets/Project/Game Logic/Abilities/Components/Actions/PulseKinematic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PulseKinematic : AbstractAbilityAction {
+
+    Rigidbody rigid;
+
+    protected override void Start() {
+        base.Start();
+        rigid = GetComponentInParent<Rigidbody>();
+    }
+
+    public override bool Activate(PhotonStream stream) {
+        rigid.isKinematic = true;
+
+        Callback.FireForUpdate(() => rigid.isKinematic = false, this);
+        return true;
+    }
+}
