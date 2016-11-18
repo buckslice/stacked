fbd032315078eea7f3d422ae371262a0e8448d25
Reddeyfish <edrichd@uci.edu>
2016-11-17 21:43:33 -0800

    Add phaseInvulnerability ability

---
+++ Assets/Project/Game Logic/Abilities/Components/Triggers/OnDamageTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OnDamageTrigger : MonoBehaviour, IUntargetedAbilityTrigger {
+
+    public event UntargetedAbilityTrigger abilityTriggerEvent = delegate { };
+
+    Health targetHealth;
+
+    void Start() {
+        targetHealth = GetComponentInParent<Health>();
+        targetHealth.onDamage += TargetHealth_onDamage;
+    }
+
+    private void TargetHealth_onDamage(float amount, int playerID) {
+        abilityTriggerEvent();
+    }
+}
--- Assets/Project/Game Logic/Enemy/BossSetupNetworkedData.cs
+++ Assets/Project/Game Logic/Enemy/BossSetupNetworkedData.cs
@@ -21,6 +21,7 @@ public class BossSetupNetworkedData : MonoBehaviour {
         KNOCKBACK,
         FLAMESWEEP,
         DEATHWAVE,
+        PHASEINVULNERABILITY,
     }
 
     public enum BossID : byte {
