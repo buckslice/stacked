6d0ffea9f3fc2c499720955c8e56666f9bf4b365
jfc3 <buckslice@gmail.com>
2016-11-19 11:46:46 -0800

    Added UI markers for offscreen dead players

---
+++ Assets/Project/Game Logic/UI/ClampToScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// used to make sure a UI element never goes offscreen
+public class ClampToScreen : MonoBehaviour {
+    RectTransform rt;
+    //CanvasScaler scaler;
+
+    // Use this for initialization
+    void Start() {
+        rt = (RectTransform)transform;
+        //scaler = transform.root.GetComponent<CanvasScaler>();
+    }
+
+    void LateUpdate() {
+        Vector2 center = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
+        Vector2 p = rt.position;
+
+        // scale UI pixel size by canvas scale
+        // I think this auto happens for sprites? commenting out for now
+        //float scale = Mathf.Lerp(
+        //    scaler.referenceResolution.x / Screen.width,
+        //    scaler.referenceResolution.y / Screen.height,
+        //    scaler.matchWidthOrHeight);
+
+        Vector2 sd = rt.sizeDelta / 2.0f;
+
+        float widthOff = Mathf.Abs(p.x - center.x);
+        float heightOff = Mathf.Abs(p.y - center.y);
+
+        // if offscreen
+        if (widthOff > center.x - sd.x || heightOff > center.y - sd.y) {
+            // dir back to center
+            Vector2 dir = new Vector2(center.x - p.x, center.y - p.y).normalized;
+
+            // find aspect ratio based off the offset
+            float aspect = (Screen.width - sd.x * 2.0f) / (Screen.height - sd.y * 2.0f);
+
+            if (widthOff / heightOff > aspect) {
+                dir *= (widthOff - center.x + sd.x) / Mathf.Abs(dir.x);
+            } else {
+                dir *= (heightOff - center.y + sd.y) / Mathf.Abs(dir.y);
+            }
+
+            transform.position = p + dir;
+        }
+    }
+
+}
---
+++ Assets/Project/Game Logic/UI/OffscreenTracker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+// right now just for seeing where dead players are but 
+// could be extended to work for identifying any sort of important thing happening offscreen
+public class OffscreenTracker : MonoBehaviour {
+
+    public GameObject deadPlayerMarker;
+
+    List<Image> markers = new List<Image>();
+    Camera cam;
+
+    // Use this for initialization
+    void Start() {
+        cam = Camera.main;
+
+        // spawn a marker for each player
+        for (int i = 0; i < Player.Players.Count; ++i) {
+            markers.Add(((GameObject)Instantiate(deadPlayerMarker, transform)).GetComponent<Image>());
+            if (i >= 0 && i < 4) {
+                markers[i].color = Player.playerColoring[Player.Players[i].PlayerID];
+            }
+            markers[i].gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        for (int i = 0; i < Player.Players.Count; ++i) {
+            if (Player.Players[i].dead) {
+                // problem if camera is further forward than position
+                Vector3 playerPos = Player.Players[i].Holder.transform.position;
+                Vector2 screenPos = cam.WorldToScreenPoint(playerPos);
+                // if position is behind camera then invert the screen coordinates
+                if (Vector3.Dot(cam.transform.forward, (playerPos - cam.transform.position).normalized) < 0) {
+                    screenPos *= -1.0f;
+                }
+
+                if (screenPos.x < 0 || screenPos.x > cam.pixelWidth ||
+                    screenPos.y < 0 || screenPos.y > cam.pixelHeight) {
+                    markers[i].gameObject.SetActive(true);
+                    markers[i].transform.position = screenPos;
+                } else {
+                    markers[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
