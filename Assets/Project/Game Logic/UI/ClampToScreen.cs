74723f190d685be635fc51545691c332e184cf66
jfc3 <buckslice@gmail.com>
2016-11-19 12:19:05 -0800

    Made boss UI group stay inside screen bounds

--- Assets/Project/Game Logic/UI/ClampToScreen.cs
+++ Assets/Project/Game Logic/UI/ClampToScreen.cs
@@ -5,45 +5,54 @@ using UnityEngine.UI;
 // used to make sure a UI element never goes offscreen
 public class ClampToScreen : MonoBehaviour {
     RectTransform rt;
-    //CanvasScaler scaler;
+    CanvasScaler scaler;
+    bool hasImageComponent = false;
 
     // Use this for initialization
     void Start() {
         rt = (RectTransform)transform;
-        //scaler = transform.root.GetComponent<CanvasScaler>();
+        scaler = transform.root.GetComponent<CanvasScaler>();
+
+        if(GetComponent<Image>()) {
+            hasImageComponent = true;
+        }
     }
 
     void LateUpdate() {
         Vector2 center = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
-        Vector2 p = rt.position;
+        Vector2 pos = rt.position;
 
-        // scale UI pixel size by canvas scale
-        // I think this auto happens for sprites? commenting out for now
-        //float scale = Mathf.Lerp(
-        //    scaler.referenceResolution.x / Screen.width,
-        //    scaler.referenceResolution.y / Screen.height,
-        //    scaler.matchWidthOrHeight);
+        Vector2 sd = rt.sizeDelta;
+        // scale size delta by canvas scale (happens automatically for images it seems)
+        if (!hasImageComponent) {
+            sd /= Mathf.Lerp(
+                scaler.referenceResolution.x / Screen.width,
+                scaler.referenceResolution.y / Screen.height,
+                scaler.matchWidthOrHeight);
+        }
 
-        Vector2 sd = rt.sizeDelta / 2.0f;
+        // undo the pivot for calculations
+        Vector2 offset = new Vector2((0.5f - rt.pivot.x) * sd.x, (0.5f - rt.pivot.y) * sd.y);
+        pos += offset;
 
-        float widthOff = Mathf.Abs(p.x - center.x);
-        float heightOff = Mathf.Abs(p.y - center.y);
+        float widthOff = Mathf.Abs(pos.x - center.x);
+        float heightOff = Mathf.Abs(pos.y - center.y);
 
         // if offscreen
-        if (widthOff > center.x - sd.x || heightOff > center.y - sd.y) {
+        if (widthOff > center.x - sd.x / 2.0f || heightOff > center.y - sd.y / 2.0f) {
             // dir back to center
-            Vector2 dir = new Vector2(center.x - p.x, center.y - p.y).normalized;
+            Vector2 dir = new Vector2(center.x - pos.x, center.y - pos.y).normalized;
 
             // find aspect ratio based off the offset
-            float aspect = (Screen.width - sd.x * 2.0f) / (Screen.height - sd.y * 2.0f);
+            float aspect = (Screen.width - sd.x) / (Screen.height - sd.y);
 
             if (widthOff / heightOff > aspect) {
-                dir *= (widthOff - center.x + sd.x) / Mathf.Abs(dir.x);
+                dir *= (widthOff - center.x + sd.x / 2.0f) / Mathf.Abs(dir.x);
             } else {
-                dir *= (heightOff - center.y + sd.y) / Mathf.Abs(dir.y);
+                dir *= (heightOff - center.y + sd.y / 2.0f) / Mathf.Abs(dir.y);
             }
-
-            transform.position = p + dir;
+            // add pivot back
+            transform.position = pos - offset + dir;
         }
     }
 
