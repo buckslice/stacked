3be28fcb36a7829b640c60577c4c07f8f0eff499
Reddeyfish <edrichd@uci.edu>
2016-11-27 09:41:46 -0800

    Only the base of a stack has a unit ring

---
+++ Assets/Project/Game Logic/Player Scripts/UnitRing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitRing : MonoBehaviour {
+
+    Stackable stackable;
+
+	void Start () {
+        stackable = GetComponentInParent<Stackable>();
+        stackable.changeEvent += Stackable_changeEvent;
+	}
+
+    private void Stackable_changeEvent() {
+        this.gameObject.SetActive(stackable.Below == null); //only active if there is nothing below us.
+    }
+
+    void Update () {
+        Vector3 flooredPosition = transform.position;
+        flooredPosition.y = 0.01f;
+        transform.position = flooredPosition;
+	}
+}
