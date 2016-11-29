3f4c393ac8914ba759e5a3c877e9997927560733
verythinice <quake.grizzly@gmail.com>
2016-11-28 17:53:08 -0800

    Fixed jumping again

--- Assets/Project/Game Logic/Abilities/Components/Constraints/TouchedGroundConstraint.cs
+++ Assets/Project/Game Logic/Abilities/Components/Constraints/TouchedGroundConstraint.cs
@@ -3,10 +3,9 @@ using UnityEngine.Assertions;
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(Collider))]
 public class TouchedGroundConstraint : UntargetedAbilityConstraint {
 
-    const float groundCheckDistance = 0.1f;
+    const float groundCheckDistance = 1f;
 
     [SerializeField]
     protected int numJumps = 2;
@@ -14,39 +13,30 @@ public class TouchedGroundConstraint : UntargetedAbilityConstraint {
     [SerializeField]
     protected int currentJumps;
 
-    //int layermask;
+    int layermask;
 
     protected override void Start() {
         base.Start();
 
         currentJumps = numJumps;
-        //layermask = LayerMask.GetMask(Tags.Layers.StaticGeometryFloor);
+        layermask = LayerMask.GetMask(Tags.Layers.StaticGeometryFloor);
     }
 
     public override bool isAbilityActivatible() {
-        return currentJumps > 0;
-        /*
         if (currentJumps > 0) {
             return true;
-        } 
-        //else {
-        //    RaycastHit info;
-        //    bool result = Physics.Raycast(this.transform.position, Vector3.down, out info, groundCheckDistance, layermask);
-        //    if(result && info.collider.CompareTag(Tags.Floor)) {
-        //        currentJumps = numJumps;
-        //        return true;
-        //    }
-        //}
+        }
+        else {
+            RaycastHit info;
+            bool result = Physics.Raycast(new Vector3(this.transform.position.x, this.transform.position.y+1, this.transform.position.z), Vector3.down, out info, groundCheckDistance, layermask);
+            if (result && info.collider.CompareTag(Tags.Floor)) {
+                currentJumps = numJumps;
+                return true;
+            }
+        }
 
         return false;
-         * */
     }
 
     public override void Activate() { currentJumps--; }
-
-    void OnTriggerEnter(Collider other) {
-        if (other.transform.CompareTag(Tags.Floor)) {
-            currentJumps = numJumps;
-        }
-    }
 }
