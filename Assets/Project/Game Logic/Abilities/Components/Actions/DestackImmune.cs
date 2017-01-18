69f9aea69dade06519462852fb0176fa385a4afb
Reddeyfish <edrichd@uci.edu>
2017-01-18 10:55:40 -0800

    Shields provide immunity to destacking

---
+++ Assets/Project/Game Logic/Abilities/Components/Actions/DestackImmune.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Interface which marks the object it is on as immune to DestroyStackAction.
+/// </summary>
+public interface IDestackImmune {
+    bool Immune { get; }
+}
+
+/// <summary>
+/// Class implementing IDestackImmune using inspector-set values.
+/// </summary>
+public class DestackImmune : MonoBehaviour, IDestackImmune {
+    [SerializeField]
+    protected bool immune = true;
+
+    bool IDestackImmune.Immune {
+        get {
+            return isActiveAndEnabled && immune;
+        }
+    }
+}
--- Assets/Project/Game Logic/Abilities/Components/Actions/DestroyStackAction.cs
+++ Assets/Project/Game Logic/Abilities/Components/Actions/DestroyStackAction.cs
@@ -12,10 +12,33 @@ public class DestroyStackAction : TypedTargetedAbilityAction {
     public override bool Activate(GameObject target, PhotonStream stream) {
         Debug.Log(target);
         Stackable stackable = target.GetComponentInParent<Stackable>();
-        if (stackable == null) { return false; }
 
-        stackable.DestackAll();
-        return true;
+        if (stream.isWriting) {
+
+            if (stackable == null) { return false; }
+
+            foreach (IDestackImmune immunity in target.transform.root.GetComponentsInChildren<IDestackImmune>()) {
+                if(immunity.Immune) {
+                    //write false if we don't destack
+                    stream.SendNext(false);
+                    //not destacking, so return immediately
+                    return true;
+                }
+            }
+
+            //write true if we do destack
+            stream.SendNext(true);
+            stackable.DestackAll();
+            return true;
+
+        } else {
+
+            //only destack if true was written
+            if((bool)stream.ReceiveNext()) {
+                stackable.DestackAll();
+            }
+            return true;
+        }
     }
 }
 
