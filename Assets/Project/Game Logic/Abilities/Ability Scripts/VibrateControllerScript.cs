efa446f549a6e9e881d7401dcb5e7d0d9522ca85
verythinice <quake.grizzly@gmail.com>
2017-01-30 05:10:59 -0800

    Finished rigging up the target dummy

---
+++ Assets/Project/Game Logic/Abilities/Ability Scripts/VibrateControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VibrateControllerScript : AbstractAbilityAction {
+
+    [SerializeField]
+    protected float vibrationStrength = 0.5f;
+
+    [SerializeField]
+    protected float vibrationDuration = 0.5f;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+    }
+
+    public override bool Activate(PhotonStream stream) {
+        foreach (ControllerPlayerInput controller in ControllerPlayerInput.allControllers) {
+            if (controller != null) {
+                controller.Vibrate(vibrationStrength, vibrationDuration);
+            }
+        }
+        return false;
+    }
+}
