7b9eb9e0dca3260a569a87faeac62ba58eb00300
verythinice <quake.grizzly@gmail.com>
2017-01-31 04:45:51 -0800

    Finished first draft of Tutorial level

---
+++ Assets/Project/Game Logic/Abilities/Components/Constraints/TrackedObjectsInactiveConstraint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackedObjectsInactiveConstraint : UntargetedAbilityConstraint {
+    [SerializeField]
+    protected GameObject[] objectsTracked;
+
+    public override void Activate() {}
+
+    public override bool isAbilityActivatible() {
+        foreach (GameObject trackedObject in objectsTracked) {
+            if (trackedObject!=null && trackedObject.activeSelf) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
