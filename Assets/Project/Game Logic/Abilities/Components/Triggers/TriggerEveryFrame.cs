9913979a1006865a6442ad481ba300c658c0707c
Reddeyfish <edrichd@uci.edu>
2017-03-07 17:43:47 -0800

    Change healing tick to be frame based, instead of time based

---
+++ Assets/Project/Game Logic/Abilities/Components/Triggers/TriggerEveryFrame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEveryFrame : MonoBehaviour, IUntargetedAbilityTrigger {
+
+    public event UntargetedAbilityTrigger abilityTriggerEvent = delegate { };
+
+    void Update() {
+        abilityTriggerEvent();
+    }
+}
