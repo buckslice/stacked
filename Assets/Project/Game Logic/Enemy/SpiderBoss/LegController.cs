7ba9df9dff3cb6bc17a742eea56ddb10a19e177e
jfc3 <buckslice@gmail.com>
2017-03-08 23:14:29 -0800

    Working on animations for spider boss

---
+++ Assets/Project/Game Logic/Enemy/SpiderBoss/LegController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegController : MonoBehaviour {
+
+    public Transform segment1;
+    public Transform segment2;
+
+    Transform root;
+
+    // Use this for initialization
+    void Start () {
+        root = transform.root;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        segment2.rotation = Quaternion.LookRotation(root.forward);
+	}
+}
