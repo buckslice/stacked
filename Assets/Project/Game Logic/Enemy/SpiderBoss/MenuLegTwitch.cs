0514103e60cc106d38f8748102707ffc66d45c5e
jfc3 <buckslice@gmail.com>
2017-03-20 17:11:30 -0700

    Swagged out main menu with scary background bosses and lightning

---
+++ Assets/Project/Game Logic/Enemy/SpiderBoss/MenuLegTwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// randomly twitch legs (used in start menu)
+public class MenuLegTwitch : MonoBehaviour {
+
+
+    float offset;
+	// Use this for initialization
+	void Start () {
+        offset = Random.value * 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localRotation = Quaternion.Euler(Mathf.PerlinNoise(Time.time*3.0f+offset, 100.0f) * 60.0f, 0.0f, 0.0f);
+	}
+}
---
+++ Assets/Project/Game Logic/UI/ScaryMenuLightning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaryMenuLightning : MonoBehaviour {
+
+    public GameObject[] models;
+
+    Light dirLight;
+
+	// Use this for initialization
+	void Start () {
+        dirLight = GetComponent<Light>();
+	}
+
+    float nextLightningTime = 5.0f;
+
+	// Update is called once per frame
+	void Update () {
+        nextLightningTime -= Time.deltaTime;
+        if(nextLightningTime < 0.0f) {
+            StartCoroutine(LightningRoutine(Random.Range(0.5f, 1.0f)));
+            nextLightningTime = Random.Range(2.0f, 5.0f);
+        }
+	}
+
+    IEnumerator LightningRoutine(float time) {
+        int index = Random.Range(0, 3);
+        models[index].SetActive(true);
+        dirLight.enabled = true;
+        float t = 0.0f;
+        while(t < time) {
+            t += Time.deltaTime;
+            dirLight.intensity = Mathf.PerlinNoise(Time.time * 10.0f,100.0f)*7.0f + 1.0f;
+            yield return null;
+        }
+        dirLight.intensity = 8.0f;
+        models[index].SetActive(false);
+        dirLight.enabled = false;
+    }
+}
